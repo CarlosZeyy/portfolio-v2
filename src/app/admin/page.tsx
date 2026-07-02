8081d366279628f69a1c52e8d0165c65ed523c9f
CarlosZeyy <carloszeeyy@gmail.com>
2026-07-02 20:48:30 -0300

    Feat: Implement Admin page with user authentication and greeting

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,21 @@
-export default function AdminPage()  {
+import { createServerSupabase } from "@/lib/supabase-server";
+import { redirect } from "next/navigation";
+
+export default async function AdminPage() {
+  const supabase = await createServerSupabase();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (!user || error) {
+    redirect("/login");
+  }
+
   return (
-    <div>Admin page</div>
-  )
+    <div>
+      <h1>Bem vindo {user.email}</h1>
+    </div>
+  );
 }
